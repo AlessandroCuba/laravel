7f22c56f32e6e489cef16d22814679b4b3640c23
Manuel A. Gonzalez Prieto <malejandro_gonzalezp@yahoo.de>
2019-07-06 18:46:30 +0200

    install debugbar

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,17 @@ Route::view('/', 'site.home', [
 ])->name('home');
 
 // Post
-Route::get('/posts', 'PostController@index')->name('posts.index');
-Route::get('/posts/create', 'PostController@create')->name('posts.create');
-Route::get('/posts/{post}/edit', 'PostController@edit')->name('posts.edit');
-Route::post('/posts', 'PostController@store')->name('posts.store');
-Route::get('/posts/{post}', 'PostController@show')->name('posts.show');
-Route::patch('/posts/{post}', 'PostController@update')->name('posts.update');
-Route::delete('/posts/{post}', 'PostController@destroy')->name('posts.delete');
+Route::get('posts', 'PostController@index')->name('posts.index');
+Route::get('posts/create', 'PostController@create')->name('posts.create');
+Route::get('posts/{post}/edit', 'PostController@edit')->name('posts.edit');
+Route::post('posts', 'PostController@store')->name('posts.store');
+Route::get('posts/{post}', 'PostController@show')->name('posts.show');
+Route::patch('posts/{post}', 'PostController@update')->name('posts.update');
+Route::delete('posts/{post}', 'PostController@destroy')->name('posts.delete');
 // end Post
 
-Route::view('/about', 'site.about')->name('about');
+Route::view('about', 'site.about')->name('about');
 
-Route::view('/contact', 'site.contact')->name('contact');
+Route::view('contact', 'site.contact')->name('contact');
 
 Route::post('contact', 'ContactController@store')->name('contact');
